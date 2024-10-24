1673e8ddb20b4f5516abf2e84c3ad0c1f891931d
Dylan Frankcom <dylanfrankcom@icloud.com>
2024-10-24 16:44:03 -0400

    Bump versions. Update .gitignore

--- moesifasgi/middleware.py
+++ moesifasgi/middleware.py
@@ -88,7 +88,7 @@ class MoesifMiddleware(BaseHTTPMiddleware):
 
     def initialize_config(self):
         Configuration.BASE_URI = self.settings.get("BASE_URI", "https://api.moesif.net")
-        Configuration.version = 'moesifasgi-python/1.0.8'
+        Configuration.version = 'moesifasgi-python/1.0.9'
         self.LOG_BODY = self.settings.get("LOG_BODY", True)
 
         self.app_config = AppConfig()
