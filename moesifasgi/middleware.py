083cffaaa28273635115aeb6d527faa637c077fb
Derric Gilling <derric@moesif.com>
2024-12-04 14:56:55 -0800

    Fix: Newer versions of FastAPI now require app as a middleware argument #188639880
    * New versions of FastAPI require the app as a middleware argument.
    * Fixes the exceptions:
    TypeError: MoesifMiddleware.__init__() got multiple values for argument 'settings'
    AttributeError: 'ExceptionMiddleware' object has no attribute 'get'
    * Update LICENSE and cleanup/remove extra code from example

--- moesifasgi/middleware.py
+++ moesifasgi/middleware.py
@@ -27,11 +27,21 @@ logger = logging.getLogger(__name__)
 
 class MoesifMiddleware(BaseHTTPMiddleware):
     """ASGI Middleware for recording of request-response"""
-    def __init__(self, settings=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, app=None, settings=None, *args, **kwargs):
+        super().__init__(app, *args, **kwargs)
 
         if settings is None:
-            raise Exception('Moesif Application ID is required in settings')
+            raise Exception('settings is not set. Ensure MoesifMiddleware is initialized with settings')
+            return
+        
+        if not isinstance(settings, dict):
+            raise Exception('settings is not a dictionary. Ensure MoesifMiddleware is initialized with a settings dictionary')
+            return
+        
+        if 'APPLICATION_ID' not in settings:
+            raise Exception('APPLICATION_ID was not defined in settings. APPLICATION_ID is a required field')
+            return
+        
         self.settings = settings
         self.DEBUG = self.settings.get('DEBUG', False)
 
